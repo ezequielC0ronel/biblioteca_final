fd783c97c3e93047e7c3e3b6ebdcfa98bb30d2b6
Ezequiel Coronel <Ezequiel Coronel@DESKTOP-2ITNFOK>
2021-06-08 10:09:35 -0300

    Se añaden mas clases Data al testeo en main.

--- src/biblioteca/principal/Biblioteca.java
+++ src/biblioteca/principal/Biblioteca.java
@@ -21,8 +21,9 @@ public class Biblioteca {
         Conexion con = new Conexion();
         //AutorData ad = new AutorData(con);
         //LibroData lbd = new LibroData(con);
-        LectorData ld = new LectorData(con);
-        
+        //LectorData ld = new LectorData(con);
+        //EjemplarData ed = new EjemplarData(con);     
+        PrestamoData pd = new PrestamoData(con);
         
         //AutorData test
         
@@ -35,12 +36,12 @@ public class Biblioteca {
         //actualizarAutor(Autor autor) - Funcionando.
         //Necesitamos un Objeto tipo Autor para poder actualizarlo, esta vez, usando el constructor que permite un id_autor.
         //El id que colocamos es el que la base le asigno a este Autor.
-        //Autor a1 = new Autor(8,38203765, "Roberto", "Fontanarrosa", "Argentina", LocalDate.of(1994, Month.NOVEMBER, 26));
+        //Autor a1 = new Autor(4,38203765, "Roberto", "Fontanarrosa", "Argentina", LocalDate.of(1994, Month.NOVEMBER, 26));
         //a1.setNombreAutor("Roberto Alfredo");
         //ad.actualizarAutor(a1);
         
         //buscarAutor(String nombre, String apellido) - Funcionando.
-        //System.out.println(ad.buscarAutor("Alejandra", "Pizarnik"));
+        //System.out.println(ad.buscarAutor("Roberto Alfredo", "Fontanarrosa"));
         //System.out.println(ad.buscarAutor("Roberto", "Martin"));//Un autor que no existe.
         
         //obtenerAutores() - Funcionando.
@@ -50,30 +51,95 @@ public class Biblioteca {
         //LibroData test
         
         //agregarLibro(Libro libro) - Funcionando.
-        //Autor a1 = new Autor(8,38203765, "Roberto Alfredo", "Fontanarrosa", "Argentina", LocalDate.of(1994, Month.NOVEMBER, 26));
+        //Autor a1 = new Autor(4,38203765, "Roberto Alfredo", "Fontanarrosa", "Argentina", LocalDate.of(1994, Month.NOVEMBER, 26));
         //Libro l1 = new Libro(a1, 734651, 1997, "Cuentos de fútbol argentino", "Alfaguara", "Ficcion");
         //lbd.agregarLibro(l1);
         
         //actualizarLibro(Libro libro) - Funcionando.
-        //Autor a1 = new Autor(8,38203765, "Roberto Alfredo", "Fontanarrosa", "Argentina", LocalDate.of(1994, Month.NOVEMBER, 26));
-        //Libro l1 = new Libro(4,a1, 734651, 1997, "Cuentos de fútbol argentino", "Alfaguara", "Ficcion");
+        //Autor a1 = new Autor(4,38203765, "Roberto Alfredo", "Fontanarrosa", "Argentina", LocalDate.of(1994, Month.NOVEMBER, 26));
+        //Libro l1 = new Libro(3,a1, 734651, 1997, "Cuentos de fútbol argentino", "Alfaguara", "Ficcion");
         //l1.setISBN(534251);
         //lbd.actualizarLibro(l1);
         
         //buscarLibro(String nombre_libro) - Funcionando.
-        /*for(Libro l :lbd.buscarLibro("Gaucho")){
+        /*for(Libro l :lbd.buscarLibro("Cuentos")){
             System.out.println(l);
-        }
-        */
+        }*/
         
         //obtenerLibros() - Funcionando.
         /*for(Libro l : lbd.obtenerLibros()){
             System.out.println(l);
             System.out.println("-----------");
         }
-       ¨*/
+        */
+        
+        
         
         //EjemplarData test
+        
+        //agregarEjemplar(Ejemplar ejemplar) - Funcionando.
+        //Autor a1 = new Autor(4,38203765, "Roberto Alfredo", "Fontanarrosa", "Argentina", LocalDate.of(1994, Month.NOVEMBER, 26));
+        //Libro l1 = new Libro(3, a1, 534251, 1997, "Cuentos de fútbol argentino", "Alfaguara", "Ficcion");
+        //Ejemplar e1 = new Ejemplar(l1, "Disponible");
+        //ed.agregarEjemplares(e1, 5);
+        
+        //actualizarEstado(Ejemplar ejemplar, String estado) - Funcionando.
+        //Ejemplar e1 = new Ejemplar(20);
+        //ed.actualizarEstado(e1, "Prestado");
+        
+        //ejemplarDisponible(int id_ejemplar) - Funcionando.
+        //System.out.println(ed.ejemplarDisponible(15));//Ejemplar disponible;
+        //System.out.println(ed.ejemplarDisponible(20));//Ejemplar no disponible;
+        
+        //estadoEjemplar(int id_ejemplar) - Funcionando.
+        //System.out.println(ed.estadoEjemplar(14));//Ejemplar con estado "Prestado"
+        
+        
+        
+        
+        //PrestamoData test
+        
+        //registrarPrestamo(Prestamo prestamo) - Funcionando.
+        /*Lector lec1 = new Lector(8,42205836, "Ezequiel", "Coronel", "Tapas 321", true);
+        Autor a1 = new Autor(3,38203765, "Roberto Alfredo", "Fontanarrosa", "Argentina", LocalDate.of(1994, Month.NOVEMBER, 26));
+        Libro l1 = new Libro(3, a1, 534251, 1997, "Cuentos de fútbol argentino", "Alfaguara", "Ficcion");
+        Ejemplar e1 = new Ejemplar(23, l1);
+        
+        Prestamo p1 = new Prestamo(lec1, e1, true, LocalDate.now());
+        
+        pd.registrarPrestamo(p1);
+        */
+        
+        //entregarPrestamo(Prestamo prestamo), modificarPrestamo(Prestamo prestamo) - Funcionando.
+        /*Lector lec1 = new Lector(8,42205836, "Ezequiel", "Coronel", "Tapas 321", true);
+        Autor a1 = new Autor(3,38203765, "Roberto Alfredo", "Fontanarrosa", "Argentina", LocalDate.of(1994, Month.NOVEMBER, 26));
+        Libro l1 = new Libro(3, a1, 534251, 1997, "Cuentos de fútbol argentino", "Alfaguara", "Ficcion");
+        Ejemplar e1 = new Ejemplar(23, l1);
+        
+        Prestamo p1 = new Prestamo(20, lec1, e1, true, LocalDate.of(2021, Month.JUNE, 8));
+        pd.entregarPrestamo(p1);
+        */
+        
+        //anularPrestamo(Prestamo prestamo) - Funcionando.
+        //Primero creamos un nuevo prestamo y luego lo registramos.
+        //Lector lec1 = new Lector(8,42205836, "Ezequiel", "Coronel", "Tapas 321", true);
+        //Autor a1 = new Autor(3,38203765, "Roberto Alfredo", "Fontanarrosa", "Argentina", LocalDate.of(1994, Month.NOVEMBER, 26));
+        //Libro l1 = new Libro(3, a1, 534251, 1997, "Cuentos de fútbol argentino", "Alfaguara", "Ficcion");
+        //Ejemplar e1 = new Ejemplar(21);
+        
+        //Prestamo p1 = new Prestamo(lec1, e1, true, LocalDate.now());
+        
+        //pd.registrarPrestamo(p1);
+        
+        //Se presenta un problema a la hora de generar el prestamo, procedemos a anular el prestamo.
+        //Lector lec1 = new Lector(8,42205836, "Ezequiel", "Coronel", "Tapas 321", true);
+        //Autor a1 = new Autor(3,38203765, "Roberto Alfredo", "Fontanarrosa", "Argentina", LocalDate.of(1994, Month.NOVEMBER, 26));
+        //Libro l1 = new Libro(3, a1, 534251, 1997, "Cuentos de fútbol argentino", "Alfaguara", "Ficcion");
+        //Ejemplar e1 = new Ejemplar(21);
+        
+        //Prestamo p1 = new Prestamo(22,lec1, e1, true, LocalDate.now()); //Esta vez usando el constructor que nos permite colocar el id_prestamo generado en la BD.
+        //pd.anularPrestamo(p1);
+        
         
     }
     
